ff7f11d0fdd54db4f6cf0a113226d5edfe41fc1b
Pujitha1107 <71667405+Pujitha1107@users.noreply.github.com>
2023-04-04 13:49:31 -0400

    Update normalize_scores.py

--- data/normalized_scores/normalize_scores.py
+++ data/normalized_scores/normalize_scores.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+
+df = pd.read_csv(filepath)
+df.insert(10, column='normalized_score', value = '')
+print(df)
+
+
+
+max_final_score = (df['domain1_score'] + df['domain2_score']).max()
+print(max_final_score)
+min_final_score = (df['domain1_score'] + df['domain2_score']).min()
+print(min_final_score)
+
+df['normalized_score'] = ((df['domain1_score'] + df['domain2_score']) - min_final_score)/(max_final_score - min_final_score)
+
+df.to_csv(filepath, index=False)
+
+print(df)
